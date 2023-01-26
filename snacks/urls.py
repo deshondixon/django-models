71892f556f6e5a018a19d9c78909628340a75bcf
deshondixon <deshondixon@gmail.com>
2023-01-25 20:38:56 -0800

    lab finsihed

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import SnackDetailView, SnackListView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", SnackListView.as_view(), name="snack_list"),
+    path("<int:pk>/", SnackDetailView.as_view(), name="snack_detail"),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,9 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView
+from .models import Snack
 
 
 # Create your views here.
-class HomePageView(TemplateView):
-    template_name = "home.html"
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
